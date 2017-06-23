669cdf1f60d3739d3b4b38c8aeaba93960da7391
Artem Bykov <artem.bikov@gmail.com>
2017-06-23 17:11:03 +0300

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'rentmania-docker'
+maintainer       'artem bykov'
+maintainer_email 'artem.bikov@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures rentmania-docker'
+long_description 'Installs/Configures rentmania-docker'
+version          '0.1.0'
+
+depends 'docker', '~> 2.0'
